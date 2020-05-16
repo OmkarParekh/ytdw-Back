a49f0fa9938b2ab9e8e962515a5c73a34593c27d
OmkarParekh <omkarparekh600@gmail.com>
2020-05-16 21:50:09 +0530

    In Process

---
+++ server.js
@@ -0,0 +1,48 @@
+const Express=require('express');
+const App=Express()
+
+// Cors Intial
+const cors =require('cors')
+
+// ytdl Intial
+const ytdl = require('ytdl-core');
+
+// Cors
+App.use(cors())
+
+// Home Page
+App.get('/',(req,res)=>{
+     res.send('hello')
+})
+
+
+// ytdl Downloader
+App.get('/i',(req,res)=>{
+     var id =req.query.id;
+     var qua =req.query.qua;
+     ytdl.getInfo(id,(err,info)=>{
+         if(err) throw err;
+         let format = ytdl.chooseFormat(info.formats, {
+          quality: qua,
+        });
+        let af = ytdl.filterFormats(info.formats, 'audioonly');
+        const obj={
+             name:info.title,
+             Thumbnail:info.player_response.microformat.playerMicroformatRenderer.thumbnail
+             .thumbnails[0].url,
+             Video_Format:format.qualityLabel,
+             Video_Url:format.url,
+             Audio_Url:af[0].url
+        }
+        // response 
+        res.send(obj) 
+    })
+    
+
+// Hosting Port 
+})
+App.listen(process.env.PORT || 3000,(req,res)=>{
+     console.log('Server is Started')
+})
+
+
